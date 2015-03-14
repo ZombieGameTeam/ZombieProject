3a094ae27146b7443a22c155d4139462a74a5918
mayk93 <mihai.mandrescu@gmail.com>
2015-03-14 16:06:40 +0200

    Directory Structure
    Added dummy files for directory structure.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,24 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+    //Plane ground;
     void Start()
     {
-
+        //ground = new Plane(Vector3.up, Vector3.zero);
     }
+
+
     void Update()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit dist;
 
+        if (Physics.Raycast(ray, out dist))
+        {
+            Vector3 clickPoint = new Vector3(ray.GetPoint(dist.distance).x, transform.position.y, ray.GetPoint(dist.distance).z);
+            transform.LookAt(clickPoint);
+        }
     }
+
+    //void 
 }
