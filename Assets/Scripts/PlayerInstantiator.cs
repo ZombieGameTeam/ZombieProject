cbdcee0001cb0752b21e652c7d29524e3d0ebf21
mayk93 <mihai.mandrescu@gmail.com>
2015-03-14 15:35:05 +0200

    Zombie Project
    Initial Commit

---
+++ Assets/Scripts/PlayerInstantiator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInstantiator : Singleton<PlayerInstantiator> 
+{
+    public GameObject playerModel;
+    public Vector3 instantationCoordinates;
+
+    protected PlayerInstantiator() { }
+
+    // Use this for initialization
+    void Start()
+    {
+        Instantiate(playerModel, instantationCoordinates, Quaternion.identity);
+    }
+}
