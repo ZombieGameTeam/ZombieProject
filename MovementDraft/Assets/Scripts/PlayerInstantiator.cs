a53a6c5cebd9c57aa0c8acd9ee4f0e800954e188
mayk93 <mihai.mandrescu@gmail.com>
2015-03-14 21:36:14 +0200

    Initial Setup
    Setup project and initial scene.
    Added player instantiation.

---
+++ MovementDraft/Assets/Scripts/PlayerInstantiator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInstantiator : Singleton<PlayerInstantiator> 
+{
+    public GameObject playerModel;
+    public Vector3 location;
+    protected PlayerInstantiator() { }
+	// Use this for initialization
+    void Start() { Instantiate(playerModel, location, Quaternion.identity); }
+}
